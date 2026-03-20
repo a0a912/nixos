8eb6f362e2be245f9b36bc09c0060a80ed027f30
aa <ahmed0ali912@gmail.com>
2026-03-20 16:26:54 -0700

    init

---
+++ flake.nix
@@ -0,0 +1,97 @@
+{
+  description = "NixOS configurations";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    nixos-hardware = {
+      url = "github:NixOS/nixos-hardware/master";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-flatpak = {
+      url = "github:gmodena/nix-flatpak/?ref=latest";
+    };
+
+    stylix = {
+      url = "github:nix-community/stylix/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-cachyos-kernel = {
+      url = "github:xddxdd/nix-cachyos-kernel/release";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      stylix,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {
+          inherit inputs;
+        };
+
+        modules = [
+          {
+            nixpkgs.config.allowUnfree = true;
+          }
+          ./configuration.nix
+          ./hardware-configuration.nix
+
+          # ./nvidia.nix
+
+          {
+            programs.steam.enable = true; # https://wiki.nixos.org/wiki/Steam
+
+            nix.settings = {
+              substituters = [ "https://cache.nixos-cuda.org" ];
+              trusted-public-keys = [ "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=" ];
+            };
+
+          }
+
+          home-manager.nixosModules.home-manager
+          # ./home-manager.nix
+
+          {
+            home-manager = {
+              backupFileExtension = "hm-backup";
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.wali = {
+                programs.mangohud = {
+                  enable = true;
+                  enableSessionWide = true;
+                };
+                home = {
+                  username = "wali";
+                  homeDirectory = "/home/wali";
+                  stateVersion = "25.11";
+                };
+              };
+            };
+          }
+
+          stylix.nixosModules.stylix
+        ];
+      };
+    };
+}
---
+++ nvidia.nix
@@ -0,0 +1,54 @@
+{ inputs, pkgs, ... }:
+{
+  imports = [
+    inputs.nixos-hardware.nixosModules.common-gpu-nvidia
+  ];
+
+  hardware.graphics.enable32Bit = true;
+
+  # NVIDIA PRIME configuration for hybrid graphics
+  hardware.nvidia = {
+    open = false;
+    modesetting.enable = true;
+    nvidiaSettings = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    prime = {
+      # Bus IDs from: lspci | grep -E "VGA|3D"
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+      offload = {
+        enable = true;
+        enableOffloadCmd = true; # Provides `nvidia-offload` command
+      };
+
+      # reverseSync.enable = true;
+    };
+  };
+
+  # disable system suspend/hibernate when using NVIDIA specialization to avoid resuming to a black screen on integrated/external displays.
+  systemd.services."systemd-suspend.service".serviceConfig.ExecStart = "${pkgs.coreutils}/bin/true";
+  systemd.services."systemd-hibernate.service".serviceConfig.ExecStart = "${pkgs.coreutils}/bin/true";
+  systemd.services."systemd-hybrid-sleep.service".serviceConfig.ExecStart =
+    "${pkgs.coreutils}/bin/true";
+
+  # boot.kernelPackages = lib.mkForce pkgs.linuxPackages;
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  boot = {
+    kernelParams = [
+      "nvidia-drm.modeset=1"
+      "nvidia-drm.fbdev=1"
+    ];
+    initrd.kernelModules = [
+      "nvidia"
+      "nvidia_modeset"
+      "nvidia_uvm"
+      "nvidia_drm"
+    ];
+  };
+
+  boot.initrd.systemd.enable = true;
+  boot.plymouth.enable = true;
+}
