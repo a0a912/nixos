58945dbfaa911b5572b3c5d3447e337edde0d19f
aa <ahmed0ali912@gmail.com>
2026-03-20 17:08:56 -0700

    added apps

--- configuration.nix
+++ configuration.nix
@@ -101,20 +101,102 @@
   environment.systemPackages = with pkgs; [
 
     # https://search.nixos.org/options?channel=25.11
+    nixfmt
+    parted
+    aria2
+
+
+    gnome-disk-utility
+
+  
+    # --- Base System & Core Utilities ---
     wget
     curl
-    nixfmt
-    heroic-unwrapped
-    lutris-unwrapped
-    vscode
-
     git
     gh
+    bat
+    eza
+    btop
+    tldr               # or 'tealdeer' for a fast Rust rewrite
+    e2fsprogs
+    ethtool
+    fakeroot
+    vim
 
+    # --- Nix Formatter ---
+    #nixfmt-rfc-style   # Modern standard nix formatter
+
+    # --- Development & Languages ---
+    clang
+    cmake
+    gnumake
+    python3
+    nodejs
+    jupyter-all
+    nodePackages.eslint
+    vscode             # or 'vscodium' for the telemetry-free version
+    sublime
+
+    # --- Browsers & Internet ---
     floorp-bin
+    brave
+    firefox
+    librewolf
+    filezilla
+    rclone
+    rclone-browser
+    transmission_4-gtk # or 'transmission_4-qt'
+    deluge
 
+    # --- Gaming & Emulation ---
+    heroic-unwrapped
+    lutris-unwrapped
+    bottles
+    protonup-qt
+    protontricks
+    protonplus
     gogdl
-    gnome-disk-utility
+    pcsx2
+    duckstation
+    ppsspp
+    melonDS
+    desmume
+    vbam
+
+    # --- Audio, Video & Graphics ---
+    ffmpeg
+    vlc
+    audacity
+    audacious
+    ocrmypdf
+    ghostscript
+
+    # --- Documents & Office ---
+    libreoffice-qt6    # or 'libreoffice-fresh'
+    abiword
+    evince
+    pdfarranger
+    foliate
+    pdfgrep
+    enscript
+
+    # --- Utilities & Tools ---
+    bitwarden-desktop
+    bleachbit
+    timeshift
+    caffeine-ng
+    hw-probe
+    piper              # Gaming mouse config
+    razergenie         # Requires OpenRazer daemon (see below)
+    cavalier           # Audio visualizer
+
+    # --- TTS / Voice ---
+    espeak-ng
+
+    # --- Flatpak Fallbacks / AppImages ---
+    # AppImages don't work natively on NixOS. If you rely on them, use this:
+    appimage-run
+  
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
